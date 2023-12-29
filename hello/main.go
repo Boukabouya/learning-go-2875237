a07572144cb8298e55a6f9bab15961fc8bcc9dee
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-29 15:01:01 +0100

    Ex2 Solve

--- hello/main.go
+++ hello/main.go
@@ -4,6 +4,30 @@ import (
 	"fmt"
 )
 
+const aConst int = 24
+
+//aConst is unexported because it starts with a lowercase letter, so it can only be accessed within the same package (main, in this case)
+
 func main() {
-	fmt.Println("Hello from Go!")
+	var aString string = "Hello from Go !"
+	fmt.Println(aString)
+	fmt.Printf("The type of this variable is %T\n ", aString)
+	var anInteger int = 9
+	fmt.Println(anInteger) //explicitly declaration
+	fmt.Printf("The type of this variable is %T\n", anInteger)
+	var defautInteger int
+	fmt.Println(defautInteger)
+	fmt.Printf("The type of this variable is %T\n", defautInteger)
+	var anotherString = "it's me Rayene" // implicitly declaration
+	fmt.Println(anotherString)
+	fmt.Printf("The variable's type is %T\n", anotherString)
+	var anotherInteger = 18
+	fmt.Println(anotherInteger)
+	fmt.Printf("The variable's type is %T\n", anotherInteger)
+	// colon equals operator :
+	myString := "This is also a string"
+	fmt.Println(myString)
+	fmt.Printf("The variable's type is %T\n", myString)
+	fmt.Println(aConst)
+	fmt.Printf("The variable's type is %T\n", aConst)
 }
