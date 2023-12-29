8575a890c6416c4a4779fdb64c6353c94a83f0df
Boukabouya <boukabouyarayane2019@gmail.com>
2023-12-29 17:32:29 +0100

    ex 3 Solve

--- hello/main.go
+++ hello/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	reader := bufio.NewReader(os.Stdin) // where the data come from (standard input)
+	fmt.Print("Entere text :")
+	input, _ := reader.ReadString('\n') /*By using _ as the variable name for the second return value, you are indicating that
+	you are not interested in handling or checking the error explicitly.
+	It's a way of saying "I know there might be an error,
+	but I'm choosing not to handle it in this part of the code."*/
+	// ReadString function requires a single character.
+	fmt.Println("your entered is :", input)
+
 }
