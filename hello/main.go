4776d5fe706295e8364ee1920e32036cd43f3bf1
Boukabouya <boukabouyarayane2019@gmail.com>
2024-01-02 09:48:36 +0100

    Write and read local text files

--- hello/main.go
+++ hello/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	fmt.Println("Write and read local text files")
+	content := "Hello from Go"
+	file, err := os.Create("./fromString.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote a file with %v characters\n", length)
+	defer file.Close() //wait until everything else is done and then execute this command. And I'll call file.Close.
+	defer readFile("./fromString.txt")
+}
+
+func readFile(fileName string) {
+	data, err := ioutil.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from the file", string(data))
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
